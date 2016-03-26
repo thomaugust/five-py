f85f58f92d8414f55f6a52973df2bc1343f9fe6b
august <thomauugust@gmail.com>
2016-03-26 18:37:02 +1100

    first commit

---
+++ five.py
@@ -0,0 +1,49 @@
+# This is a python copy of the original five.js
+# Found at https://github.com/jackdcrawford/five.git
+
+import random
+import math
+import time
+
+# d8888b.  .d8b.  d8b   db d8888b.  .d88b.  .88b  d88.
+# 88  `8D d8' `8b 888o  88 88  `8D .8P  Y8. 88'YbdP`88
+# 88oobY' 88ooo88 88V8o 88 88   88 88    88 88  88  88
+# 88`8b   88~~~88 88 V8o88 88   88 88    88 88  88  88
+# 88 `88. 88   88 88  V888 88  .8D `8b  d8' 88  88  88
+# 88   YD YP   YP VP   V8P Y8888D'  `Y88P'  YP  YP  YP
+#
+#
+
+def five():
+    print 5
+def roman():
+    print 'V'
+def string():
+    print "five"
+def ru():
+    print "russian five"
+
+# .88b  d88.  .d8b.  d888888b db   db .d8888.
+# 88'YbdP`88 d8' `8b `~~88~~' 88   88 88'  YP
+# 88  88  88 88ooo88    88    88ooo88 `8bo.
+# 88  88  88 88~~~88    88    88~~~88   `Y8b.
+# 88  88  88 88   88    88    88   88 db   8D
+# YP  YP  YP YP   YP    YP    YP   YP `8888Y'
+#
+#
+
+def plus(int):
+    sum = 5 + int
+    print sum
+def minus(int):
+    sum = 5 - int
+    print sum
+def times(int):
+    sum = 5 * int
+    print sum
+def divide(int):
+    sum = 5 / int
+    print sum
+def power(int):
+    power = math.pow(5,int)
+    print power
---
+++ test.py
@@ -0,0 +1,10 @@
+# Test file for five functions
+
+import five
+
+five.five()
+five.roman()
+five.plus(2)
+five.minus(2)
+five.times(2)
+five.power(2)
